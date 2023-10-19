e717e1e83223bc50c32a9403d4964671cfe843f3
yavordobrev24 <yavordobrev24@gmail.com>
2023-10-19 11:52:57 +0300

    Configured database
    Configuration of mongodb using mongoose.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 const constants = {
   PORT: 3000,
+  CONNECTION_STR: "mongodb://127.0.0.1:27017/web-diary",
 };
 module.exports = constants;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
 const app = express();
 
-const { PORT } = require("./constants");
+const { CONNECTION_STR, PORT } = require("./constants");
 
 app.use(express.urlencoded({ extended: false }));
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
+
+async function connectDb() {
+  await mongoose.connect(CONNECTION_STR);
+  console.log("Connected to DB");
+}
+
+connectDb();
+app.get("/", (req, res) => {
+  res.send("It works!");
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
